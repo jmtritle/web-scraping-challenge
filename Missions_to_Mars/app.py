1f9c3508b57673465343002e5824874702df7350
jmtritle <57117317+jmtritle@users.noreply.github.com>
2020-03-10 15:15:05 -0400

    even more adjustments

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, redirect 
 from flask_pymongo import PyMongo
+from jinja2 import Template
 import scrape_mars
 
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri = 'mongodb://localhost:27017')
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_mission'
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
