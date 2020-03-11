29aedd6a29eef432fae3fbd77c106e03c8fc3f45
jmtritle <57117317+jmtritle@users.noreply.github.com>
2020-03-10 21:07:02 -0400

    yes more updates woo

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template, redirect 
 from flask_pymongo import PyMongo
+import pymongo
 from jinja2 import Template
 import scrape_mars
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_mission'
-mongo = PyMongo(app)
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars_info
+collection = db.mars_data
 
 @app.route('/')
 def home():
-    mars_info = mongo.db.mars_info.find_one()
+    mars_info = client.db.mars_info.find_one()
     return render_template('index.html', mars_info = mars_info)
 
 @app.route('/scrape')
 def scrape():
-    mars_info = mongo.db.mars_info
+    mars_info = client.db.mars_info
     mars_data = scrape_mars.scrape_news()
     mars_data = scrape_mars.scrape_image()
     mars_data = scrape_mars.scrape_weather()
     mars_data = scrape_mars.scrape_facts()
     mars_data = scrape_mars.scrape_hemi()
-    mars_info.update({}, mars_data, upsert = True)
+    db.collection.updateOne({},mars_data,{upsert: True})
     return redirect('/')
 
 if __name__ == "__main__":
